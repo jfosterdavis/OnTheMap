b1522b875fc4fba471c41d9ccf577265c79bcf00
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2016-09-17 05:06:34 -0400

    buttons now turn random colors.  just for fun.

--- OnTheMap/BorderedButton.swift
+++ OnTheMap/BorderedButton.swift
@@ -23,6 +23,23 @@ class BorderedButton: UIButton {
     let borderedButtonCornerRadius: CGFloat = 4.0
     let phoneBorderedButtonExtraPadding: CGFloat = 14.0
     
+    let randoColorOptions = [UIColor(red: 51.0/255.0, green: 204.0/255.0, blue: 255.0/255.0, alpha:1.0),
+                             UIColor(red: 51.0/255.0, green: 102.0/255.0, blue: 255.0/255.0, alpha:1.0),
+                             UIColor(red: 102.0/255.0, green: 51.0/255.0, blue: 255.0/255.0, alpha:1.0),
+                             UIColor(red: 204.0/255.0, green: 51.0/255.0, blue: 255.0/255.0, alpha:1.0),
+                             UIColor(red: 51.0/255.0, green: 255.0/255.0, blue: 204.0/255.0, alpha:1.0),
+                             UIColor(red: 0.0/255.0, green: 184.0/255.0, blue: 245.0/255.0, alpha:1.0),
+                             UIColor(red: 0.0/255.0, green: 138.0/255.0, blue: 184.0/255.0, alpha:1.0),
+                             UIColor(red: 255.0/255.0, green: 51.0/255.0, blue: 204.0/255.0, alpha:1.0),
+                             UIColor(red: 51.0/255.0, green: 255.0/255.0, blue: 102.0/255.0, alpha:1.0),
+                             UIColor(red: 184.0/255.0, green: 46.0/255.0, blue: 0.0/255.0, alpha:1.0),
+                             UIColor(red: 245.0/255.0, green: 61.0/255.0, blue: 0.0/255.0, alpha:1.0),
+                             UIColor(red: 255.0/255.0, green: 51.0/255.0, blue: 102.0/255.0, alpha:1.0),
+                             UIColor(red: 102.0/255.0, green: 255.0/255.0, blue: 51.0/255.0, alpha:1.0),
+                             UIColor(red: 204.0/255.0, green: 255.0/255.0, blue: 51.0/255.0, alpha:1.0),
+                             UIColor(red: 255.0/255.0, green: 204.0/255.0, blue: 51.0/255.0, alpha:1.0),
+                             UIColor(red: 255.0/255.0, green: 102.0/255.0, blue: 51.0/255.0, alpha:1.0)]
+    
     var backingColor: UIColor? = nil
     var highlightedBackingColor: UIColor? = nil
     
@@ -41,9 +58,9 @@ class BorderedButton: UIButton {
     fileprivate func themeBorderedButton() {
         layer.masksToBounds = true
         layer.cornerRadius = borderedButtonCornerRadius
-        highlightedBackingColor = darkerBlue
-        backingColor = lighterBlue
-        backgroundColor = lighterBlue
+        highlightedBackingColor = getRandoColor()
+        backingColor = getRandoColor()
+        backgroundColor = getRandoColor()
         setTitleColor(UIColor.white, for: UIControlState())
         titleLabel?.font = UIFont.systemFont(ofSize: titleLabelFontSize)
     }
@@ -52,29 +69,29 @@ class BorderedButton: UIButton {
     
     fileprivate func setBackingColor(_ newBackingColor: UIColor) {
         if let _ = backingColor {
-            backingColor = newBackingColor
-            backgroundColor = newBackingColor
+            backingColor = getRandoColor()
+            backgroundColor = getRandoColor()
         }
     }
     
     fileprivate func setHighlightedBackingColor(_ newHighlightedBackingColor: UIColor) {
-        highlightedBackingColor = newHighlightedBackingColor
-        backingColor = highlightedBackingColor
+        highlightedBackingColor = getRandoColor()
+        backingColor = getRandoColor()
     }
     
     // MARK: Tracking
     
     override func beginTracking(_ touch: UITouch, with withEvent: UIEvent?) -> Bool {
-        backgroundColor = highlightedBackingColor
+        backgroundColor = getRandoColor()
         return true
     }
     
     override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
-        backgroundColor = backingColor
+        backgroundColor = getRandoColor()
     }
     
     override func cancelTracking(with event: UIEvent?) {
-        backgroundColor = backingColor
+        backgroundColor = getRandoColor()
     }
     
     // MARK: Layout
@@ -85,5 +102,11 @@ class BorderedButton: UIButton {
         sizeThatFits.width = super.sizeThatFits(size).width + extraButtonPadding
         sizeThatFits.height = borderedButtonHeight
         return sizeThatFits
+    }
+    
+    //adapted from http://stackoverflow.com/questions/24003191/pick-a-random-element-from-an-array
+    func getRandoColor() -> UIColor {
+        let randomIndex = Int(arc4random_uniform(UInt32(self.randoColorOptions.count)))
+        return randoColorOptions[randomIndex]
     }
 }
